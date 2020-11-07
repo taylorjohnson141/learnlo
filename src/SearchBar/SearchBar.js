0758b4cb0cce31ce7892496a31f9fca59c920375
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-07 14:11:43 -0700

    Add handle change method to SearchBar

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react'
 class SearchBar extends Component{
   constructor(){
     super()
+    this.state = {
+     currentWord :''
+    }
   }
-  
+  handleChange = (event) =>{
+    this.setState({currentWord:event.target.value})
+  }
+  render(){
+    return(
+      <form onSubmit = {this.handleSubmit}>
+        <label>
+          Input a word would you like to Study!
+          <input type="text" value = {this.currentWord} onChange = {this.handleChange} />
+      </label>
+      </form>
+    )
+  }
 }

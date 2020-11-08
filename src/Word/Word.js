a42229b0de931ec3bc6a71cb5c94175a19d9731f
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-07 19:59:00 -0700

    Add onClick event to word card

--- src/Word/Word.js
+++ src/Word/Word.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
  function Word (props){
    console.log('word is the word', props.currentWord)
-   if(!props.currentWord ){
-     return <h1>Add a word!</h1>
-   }
+   
   return(
-    <section>
+    <section onClick = {props.addWord}>
     {props.currentWord.hwi.hw}
     English Transaltion
     {props.currentWord.shortdef[0]}

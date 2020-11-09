8809bee52c161964a317e6dc710cf260f5c8a293
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-08 18:43:34 -0700

    Add conditional for cards without proonciation

--- src/Word/Word.js
+++ src/Word/Word.js
@@ -4,14 +4,21 @@ import './Word.css'
    if(!props.currentWord){
     return <h1>Add a word</h1>
    }
-   let audioFile = props.currentWord.hwi.prs[0].sound.audio
-   let audioSubDirect = audioFile.charAt(0)
+   let audio;
+   if(props.currentWord.hwi.prs){
+     let audioFile = props.currentWord.hwi.prs[0].sound.audio
+     let audioSubDirect = audioFile.charAt(0)
+     audio = 
+     <audio controls>
+  <source src={`https://media.merriam-webster.com/audio/prons/es/me/mp3/${audioSubDirect}/${audioFile}.mp3`}/>
+</audio>
+   }
+   
+   
    console.log('word is the word', props.currentWord  )
   return(
     <section class ='word-card'>
- <audio controls>
-  <source src={`https://media.merriam-webster.com/audio/prons/es/me/mp3/${audioSubDirect}/${audioFile}.mp3`}/>
-</audio>
+ {audio}
     <p1>Word: {props.currentWord.hwi.hw}</p1>
     <p1>English Transaltion:
     {props.currentWord.shortdef[0]}

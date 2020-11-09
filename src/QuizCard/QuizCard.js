4ce818744d1e3b0b9c83e2fe6732be9a502b0fda
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-08 21:34:00 -0700

    Fix minor typo in quizcard

--- src/QuizCard/QuizCard.js
+++ src/QuizCard/QuizCard.js
@@ -2,11 +2,11 @@ function QuizCard (props){
   console.log('I am here')
   return (
     <section>
-    <h1>What is the English translation of ${props.currentWord.hwi.hw}</h1>
+    <h1>What is the English translation of {props.currentWord.hwi.hw}</h1>
     <button> Weird</button>
         <button onClick ={() =>{
           props.changeCurrentWord()
-        }}>${props.currentWord.shortdef[0]}</button>
+        }}>{props.currentWord.shortdef[0]}</button>
         <button>No</button>
         <button>Something</button>
         </section>

90be4c9a57a3eeab6a0d8fb05e03293d2cbfbc6a
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-09 14:18:47 -0700

    Add HomeScreen component

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -2,9 +2,9 @@ import './HomeScreen.css'
 function HomeScreen (){
   return (
     <section>
-    <h1 className = 'center'>Learn Spanish With LearnLo!</h1>
-    <h1 className = 'center'>Choose some words to study! </h1>
-    <h1 className = 'center'>Take quizzes and progress!</h1>
+    <h1 className = 'center-home'>Learn Spanish With LearnLo!</h1>
+    <h1 className = 'center-home'>Choose some words to study! </h1>
+    <h1 className = 'center-home'>Take quizzes and progress!</h1>
     </section>
   )
 }

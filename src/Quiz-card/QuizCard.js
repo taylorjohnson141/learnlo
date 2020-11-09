172ac5e8243cdd0ded9b94519baae20309817cd1
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-08 20:19:23 -0700

    Create quiz card

---
+++ src/Quiz-card/QuizCard.js
@@ -0,0 +1,15 @@
+function QuizCard (props){
+  return (
+    <section>
+    <h1>What is the English translation of ${props.currentWord.hwi.hw}</h1>
+    <button> Weird</button>
+        <button onClick ={() =>{
+          props.changeCurrentWord()
+        }}>${props.currentWord.shortdef[0]}</button>
+        <button>No</button>
+        <button>Something</button>
+        </section>
+  )
+ 
+  
+}

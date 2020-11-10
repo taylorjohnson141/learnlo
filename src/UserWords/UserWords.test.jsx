4150115b2a7774d4e1ce44bee0422b0d0855e6b8
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-09 17:57:30 -0700

    Add UserWords tests

---
+++ src/UserWords/UserWords.test.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import UserWords from './UserWords'
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('UserWords', () =>{
+  let currentWords;
+  beforeEach(() => {
+    currentWords = [{
+      hwi:{
+        hw:"lejos",
+        prs:[{sound: {audio: "lejos01sp"}}]
+      },
+      shortdef:[ "far away, distant",
+      "long ago, a long way off",
+      "by far"]
+    },{
+      hwi:{
+      hw: "algo",
+      prs:[{sound: {audio: "algo001sp"}}],
+      },
+      shortdef: ["somewhat, rather"]
+  }
+  
+  ]
+    })
+  it('should render correct message if there are no words', () =>{
+    render(<UserWords words = {[]}/>)
+    let NoWordsMessage = screen.getByText('No Words Yet!')
+    expect(NoWordsMessage).toBeInTheDocument()
+  })
+  it('should render SearchBar even if there are no words', () =>{
+    render(<UserWords words = {[]}/>)
+    let SearchBar = screen.getByText('Input a word you would like to Study!')
+    expect(SearchBar).toBeInTheDocument()
+  })
+  it('should render correct words when are words', () =>{
+    render(<UserWords words = {currentWords}/>)
+    let far = screen.getByText('lejos')
+    let something = screen.getByText('algo')
+    expect(far).toBeInTheDocument()
+    expect(something).toBeInTheDocument()
+  })
+})

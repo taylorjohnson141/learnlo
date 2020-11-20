8a1414f8ea0bc5df0dbf88bdab82b409bc02c121
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-20 14:35:45 -0700

    take out tests that are based on random shuffling

--- src/Quiz/Quiz.test.jsx
+++ src/Quiz/Quiz.test.jsx
@@ -20,37 +20,30 @@ describe('Quiz', () =>{
       prs:[{sound: {audio: "algo001sp"}}],
       },
       shortdef: ["somewhat, rather"]
-  }
-  
+  },
+  {
+    hwi:{
+      hw:"gato",
+      prs:[{sound: {audio: "gato"}}]
+    },
+    shortdef:[ "cat"]
+  },{
+  hwi:{
+    hw:"perro",
+    prs:[{sound: {audio: "dog"}}]
+  },
+  shortdef:[ "dog"]
+}
   ]
     })
   it('Should Render the correct message if there are no words' ,() =>{
     render(<Quiz favoriteWords = {[]}/>)
-    let noWordsMessage = screen.getByText('Input a word you would like to Study!')
+    let noWordsMessage = screen.getByText('No Words to Study Yet!')
     expect(noWordsMessage).toBeInTheDocument()
   })
   it('Should Render QuizCards when given words', () =>{
     render(<Quiz favoriteWords = {favoriteWords}/>)
     let cardQuestion= screen.getByText('What is the English translation of lejos?')
     expect(cardQuestion).toBeInTheDocument()
-  })
-  it('Should renderScore when right cards are clicked',async () =>{
-    render(<Quiz favoriteWords = {favoriteWords}/>)
-    let cardQuestion = screen.getByText("far away, distant")
-    userEvent.click(cardQuestion)
-
-    let cardQuestion2= await waitFor( () =>screen.getByText('somewhat, rather'))
-    userEvent.click(cardQuestion2)
-    let score = await waitFor ( () => screen.getByText('Your final score is 100%'))
-    expect(score).toBeInTheDocument()
-  })
-  it('Should render correct Score when user is wrong cards are clicked', async () =>{
-    render(<Quiz favoriteWords = {favoriteWords}/>)
-    let cardQuestion = screen.getByText("far away, distant")
-    userEvent.click(cardQuestion)
-    let cardQuestion2= await waitFor( () =>screen.getByText('Weird'))
-    userEvent.click(cardQuestion2)
-    let score = await waitFor ( () => screen.getByText('Your final score is 50%'))
-    expect(score).toBeInTheDocument()
   })
 })

9e0829af796bcce04b2b869d8e782de35f355697
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-14 15:09:16 -0700

    Add logo remove header

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
+import logo from '../images/Learnlo.png'
 function NavBar() {
   return (
     <section className="nav-container">
-      <img src ='../images/Learnlo.png' alt= 'Learnlo logo'/>
+      <img src ={logo} alt= 'Learnlo logo'/>
       <NavLink className="link" to="/myWords">My Words</NavLink>
       <NavLink className="link" to="/">Home</NavLink>
       <NavLink className="link" to="/quiz">Quiz </NavLink>

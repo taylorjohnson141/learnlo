35135ae55c088c680df88a68af5919e310fb4716
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-14 15:04:43 -0700

    Add logo file

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
-
 function NavBar() {
   return (
     <section className="nav-container">
+      <img src ='../images/Learnlo.png' alt= 'Learnlo logo'/>
       <NavLink className="link" to="/myWords">My Words</NavLink>
       <NavLink className="link" to="/">Home</NavLink>
       <NavLink className="link" to="/quiz">Quiz </NavLink>

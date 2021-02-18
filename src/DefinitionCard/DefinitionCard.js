d09c40f6c4e9577a8d671c526ea3b83e32ebc258
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-18 16:01:01 -0700

    Add Definition card to homeScreen

---
+++ src/DefinitionCard/DefinitionCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function DefinitionCard (props){
+  return (
+    <section>
+      {props.word}
+    </section>
+  )
+}
+export default DefinitionCard
--- src/HomeScreen/HomeScreen.jsx
+++ src/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar'
+import DefinitionCard from '../DefinitionCard/DefinitionCard'
 import './HomeScreen.css';
 
 function HomeScreen(props) {
   return (
     <section>
        <SearchBar addWord={props.addWord} />
+       {props.word &&
+    <DefinitionCard word = {props.word}/>
+       }
        {/* need a word and definition here */}
     </section>
   );

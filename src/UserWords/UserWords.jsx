a0cba1e054ac53008896e420a15a291059af1412
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-25 18:07:03 -0700

    Changes to searhbar css

--- src/UserWords/UserWords.jsx
+++ src/UserWords/UserWords.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 
 function WordList({words,deleteWord}){
   if(words.length === 0){
-    return 'Add Some Words!'
+    return <section className = "zero-words">Add Some Words! </section>
   }
   return words.map((word,index)=>{
     return <div key = {index}><Word  deleteWord = {deleteWord} currentWord = {word}/></div>

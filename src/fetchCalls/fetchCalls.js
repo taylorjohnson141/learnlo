9039dc5b58fcb3769b7852c1cfc2d65579a572a4
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-27 16:29:49 -0700

    Add second call for translated word

--- src/fetchCalls/fetchCalls.js
+++ src/fetchCalls/fetchCalls.js
@@ -1,5 +1,28 @@
+import { object } from "prop-types"
+
 export const getWord = async (word) =>{
   let response = await fetch(`https://www.dictionaryapi.com/api/v3/references/spanish/json/${word}?key=${process.env.REACT_APP_API_KEY}`)
   let definition = await response.json()
+  if(typeof definition[0] !== object){
+    return "Sorry we can't find that word"
+  }
+  let lang = definition[0].meta.lang
+  let nextCal  = definition[0].def[0].sseq[0][0][1].dt[0][1]
+  let pipeIndex = nextCal.indexOf('|')
+  let endCurlyIndex;
+  for(let i = pipeIndex; i<nextCal.length; i++){
+    if(nextCal.charAt(i) === '}'){
+      endCurlyIndex = i
+      break
+    }
+  }
+  let nextQueryWord = nextCal.slice(pipeIndex+1,endCurlyIndex)
+  let secondResponse = await fetch(`https://www.dictionaryapi.com/api/v3/references/spanish/json/${nextQueryWord}?key=${process.env.REACT_APP_API_KEY}`)
+  let secondDefinition = secondResponse.json()
+  let secondLang = secondDefinition[0].meta.lang
+  let words = {
+    [lang] : definition[0],
+    [secondLang] : secondDefinition[0]
+  }
 return definition[0]
 }

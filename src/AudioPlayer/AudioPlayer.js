e0818638a4173bca88d2c2e2f2f53fd69646bee3
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-28 14:01:26 -0700

    Add AudioPlayer outline

---
+++ src/AudioPlayer/AudioPlayer.js
@@ -0,0 +1,24 @@
+import useState from 'react'
+
+function AudioPlayer (props) {
+  let [playing, updatePlaying] = useState(false)
+  const audioElement = new Audio(props.src);
+
+return (
+    <>
+ 
+  <button onClick ={ () =>{
+    if(!playing){
+      audioElement.play();
+      updatePlaying(true)
+    }
+    else{
+      audioElement.pause()
+      updatePlaying(true)
+
+    }
+  }}>
+  </button>
+  </>
+)
+}

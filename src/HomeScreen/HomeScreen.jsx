15f82c9d705909975852f2eaa264cdf8820a32f3
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-28 15:55:58 -0700

    Add hero banner

--- src/HomeScreen/HomeScreen.jsx
+++ src/HomeScreen/HomeScreen.jsx
@@ -5,10 +5,10 @@ import './HomeScreen.css';
 
 function HomeScreen(props) {
   return (
-    <section>
+    <section className = 'homescreen'>
        <SearchBar displayWord = {props.displayWord} />
        {props.currentWord &&
-    <DefinitionCard addWord={props.addWord} word = {props.currentWord}/>
+      <DefinitionCard addWord={props.addWord} word = {props.currentWord}/>
        }
        {props.error && <section className = 'error'>
          {props.error}
